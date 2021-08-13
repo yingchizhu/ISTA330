5e4ffb462cb93e9c0fcbe3116d4ebb7dcb6312c6
habahram <habahram@email.arizona.edu>
2021-08-12 23:11:52 -0400

    initial setup

---
+++ index.md
@@ -0,0 +1,16 @@
+## Advanced Web Design - Spring 2021
+
+
+
+### Practicum
+
+1. [Week 1](practicum/week-1/index.html)
+2. [Week 2](practicum/week-2/index.html)
+3. [Week 3](practicum/week-3/index.html)
+4. [Week 4](practicum/week-4/index.md)
+5. [Week 5](practicum/week-5/index.html)
+6. [Week 6](practicum/week-6/index.html)
+7. [Week 7](practicum/week-7/index.html)
+8. [Week 8](practicum/week-8/index.html)
+
+
---
+++ practicum/week-2/question12.js
@@ -0,0 +1,19 @@
+/*
+A string, input, which contains only letters a and b is given.
+A list of substrings s1, s2, .., sn is called a partition for input if and only if
+  input == s1 + s2 + ... + sn.
+
+  
+A substring is balanced if it has equal number of a's and b's.
+The number of balanced substrings in a partition is called the balence number of the partition.
+Among all the possible partitions of the input string, what is the maximum balance number?
+
+Example:
+input: 'abaabbabab'
+output: 4 because the following partition has the highest number of balanced substrings:
+          'ab', 'aabb', 'ab', 'ab'
+*/
+
+var maxBalanceNumber = function(input) {
+
+};
