47b1eb0fdc3fcdac2f37585f8d99bfca7e9a055d
yingchizhu <87506263+yingchizhu@users.noreply.github.com>
2021-09-10 13:48:18 -0700

    Update question7.js

--- practicum/week-2/question7.js
+++ practicum/week-2/question7.js
@@ -1,27 +1,26 @@
 /*
-Given an array of 2n numbers, group the numbers into n pairs of integer,
-such that the sum of the minimum of each pair is the largest.
+Given an array of numbers, for each number in the array 
+find out how many numbers in the array are both even and bigger than the number.
 
-Example: 
-input: [4, 3, 8, 0]
-output: 4
-       because we can make the following pairs: (4, 8) and (3, 0) and 
-       min(4,8) + min(3,0) = 4
-       whatever other pairs we choose the sum of the minimums will not exceed 4.
+Example:
+input: [23, 44, 12, 4]
+output: [1, 0, 1, 2]
 */
 
-var maxSumOfMins = function(input) {
-       input.sort(function sortNumber(a, b) {
-              return a - b
-       });
-       let count =0;
-       for(i=0;i<input.length;i++){
-              if (i % 2 === 0) {
-                     count +=input[i];
-              }
-              
-       }
-       return count;
+var biggerAndEven = function(input) {
+    let myArray=new Array()
+    let count =0;
+    for (var i = 0;i< (input.length); i++){
+        count=0;
+        for(var j = 0;j<(input.length); j++){
+            if (i!=j){
+                if(input[i]<input[j] && input[j]%2 ==0){
+                    count +=1
+                }
+            }
+        }
+        myArray.push(count);
 
-
+    }
+    return myArray;
 };
