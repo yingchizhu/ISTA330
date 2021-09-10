0d113b1974047671ec3ba80c4f99a85b5346c06e
yingchizhu <87506263+yingchizhu@users.noreply.github.com>
2021-09-10 13:49:37 -0700

    Update question9.js

--- practicum/week-2/question9.js
+++ practicum/week-2/question9.js
@@ -9,5 +9,11 @@ output: 'hello'
 */
 
 var suffleString = function(input, shuffleIndices) {
-
+    let curr_index=0;
+    let word= [];
+    for (i = 0;i<input.length;i++){
+        curr_index=shuffleIndices[i];
+        word[curr_index] = input[i];
+    }
+    return word.join('');
 };
