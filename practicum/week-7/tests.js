3381e2f3384e291e15c17a6914e1af0a140df4dd
habahram <habahram@email.arizona.edu>
2021-10-16 19:38:49 -0700

    practicum 7 is posted.

--- practicum/week-7/tests.js
+++ practicum/week-7/tests.js
@@ -11,78 +11,79 @@ window.onload = () => {
     //let api = `http://localhost:4002`;
     let testTaker = names[random];
     let testTakerEmail = testTaker + '@gmail.com';
-    let password = 123;   
+    let password = 123;
     let quizId = -1;
     let testDiv = document.getElementById('test-the-api');
     testDiv.innerHTML += `<h2>****************************************************************************************</h2>`;
     testDiv.innerHTML += `<h2>Testing the API at ${api} ...</h2>`;
-     fetch(`${api}/flowers`).then(x => x.json()).
-     then(x => testDiv.innerHTML += `<h2>There are ${x.length} flowers in the database.</h2>`)
-     .catch(e => testDiv.innerHTML += `<h2>Error in /flowers method: ${e}</h2>`);
-     fetch(`${api}/quizzes`).then(x => x.json()).
-     then(x => testDiv.innerHTML += `<h2>There are ${x.length} quizzes in the database.</h2>`)
-     .catch(e => testDiv.innerHTML += `<h2>Error in /quizzes method: ${e}</h2>`);
-     fetch(`${api}/register`, {
-         method: 'POST',
-         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-         },
-         body: JSON.stringify({
-             name: testTaker,
-             email: testTakerEmail,
-             password: password
-         })
-     }).then(x => {
-         if(x.status == 200) {
-            return x.json();
-         } else {
-             throw Error(x.message);
-         }         
-        }).
-     then(x => testDiv.innerHTML += `<h2>The test taker ${testTakerEmail} registered successfully.</h2>`)
-     .catch(e => testDiv.innerHTML += `<h2>Error in /register method: ${e}</h2>`)
-     .then(() => fetch(`${api}/login`, {
+    fetch(`${api}/flowers`).then(x => x.json()).
+        then(x => testDiv.innerHTML += `<h2>There are ${x.length} flowers in the database.</h2>`)
+        .catch(e => testDiv.innerHTML += `<h2>Error in /flowers method: ${e}</h2>`);
+    fetch(`${api}/quizzes`).then(x => x.json()).
+        then(x => testDiv.innerHTML += `<h2>There are ${x.length} quizzes in the database.</h2>`)
+        .catch(e => testDiv.innerHTML += `<h2>Error in /quizzes method: ${e}</h2>`);
+    fetch(`${api}/register`, {
         method: 'POST',
-         headers: {
+        headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-         },
-         body: JSON.stringify({             
-             email: testTakerEmail,
-             password: password
-         })
-     }))
-     .then(x => x.json())
-     .then(x => testDiv.innerHTML += (x.isvalid ? `<h2>The test taker ${testTakerEmail} logged in successfully.</h2>` : 
-     `<h2>The provided credentials are invalid.</h2>`))
-     .catch(e => testDiv.innerHTML += `<h2>Error in /login method: ${e}</h2>`)
-     .then(() => fetch(`${api}/quizzes`))
-     .then(x => x.json())
-     .then(x => {
-         quizId = x[Math.floor(Math.random() * x.length)].id; 
-         return x;
+        },
+        body: JSON.stringify({
+            name: testTaker,
+            email: testTakerEmail,
+            password: password
         })
-     .then(x => fetch(`${api}/score`, {
-        method: 'POST',
-         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-         },
-         body: JSON.stringify({             
-             quizTaker: testTakerEmail,
-             quizId: quizId,
-             score: 5
-         })
-     }))
-     .then(x => x.json())
-     .then(x => testDiv.innerHTML += `<h2>The score for the test taker ${testTakerEmail} for quiz ${quizId} was submitted.</h2>`)
-     .catch(e => testDiv.innerHTML += `<h2>Error in /score post method: ${e}</h2>`)     
-     .then(x => fetch(`${api}/scores/${testTakerEmail}/${quizId}`))
-     .then(x => x.json())
-     .then(x => testDiv.innerHTML += `<h2>The test taker ${testTakerEmail} has ${x.length} submitted scores for quiz ${quizId}.</h2>`)
-     .catch(e => testDiv.innerHTML += `<h2>Error in /scores get method: ${e}</h2>`)
-     .then();
+    }).then(x => {
+        if (x.status == 200) {
+            testDiv.innerHTML += `<h2>The test taker ${testTakerEmail} registered successfully.</h2>`;
+        } else if (x.status == 403) {
+            testDiv.innerHTML += `<h2>A test taker with the same email address ${testTakerEmail} already exists.</h2>`;
+        } else {
+            testDiv.innerHTML += `<h2>The registration request failed. Status code is ${x.status}</h2>`;
+        }
+    })
+        .catch(e => testDiv.innerHTML += `<h2>Error in /register method: ${e}</h2>`)
+        .then(() => fetch(`${api}/login`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: testTakerEmail,
+                password: password
+            })
+        }))
+        .then(x => x.json())
+        .then(x => testDiv.innerHTML += (x.isvalid ? `<h2>The test taker ${testTakerEmail} logged in successfully.</h2>` :
+            `<h2>The provided credentials are invalid.</h2>`))
+        .catch(e => testDiv.innerHTML += `<h2>Error in /login method: ${e}</h2>`)
+        .then(() => fetch(`${api}/quizzes`))
+        .then(x => x.json())
+        .then(x => {
+            quizId = x[Math.floor(Math.random() * x.length)].id;
+            return x;
+        })
+        .then(x => fetch(`${api}/score`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                quizTaker: testTakerEmail,
+                quizId: quizId,
+                score: 5
+            })
+        }))
+        .then(x => x.json())
+        .then(x => testDiv.innerHTML += `<h2>The score for the test taker ${testTakerEmail} for quiz ${quizId} was submitted.</h2>`)
+        .catch(e => testDiv.innerHTML += `<h2>Error in /score post method: ${e}</h2>`)
+        .then(x => fetch(`${api}/scores/${testTakerEmail}/${quizId}`))
+        .then(x => x.json())
+        .then(x => testDiv.innerHTML += `<h2>The test taker ${testTakerEmail} has ${x.length} submitted scores for quiz ${quizId}.</h2>`)
+        .catch(e => testDiv.innerHTML += `<h2>Error in /scores get method: ${e}</h2>`)
+        .then();
     let iframe = document.getElementById('myApplicationFrame');
     iframe.setAttribute('src', api);
 }
