1a2da8d3aafa63c521a3e0fa195a58628f74323a
habahram <habahram@email.arizona.edu>
2021-10-20 12:01:53 -0700

    practicum 7 is posted.

--- practicum/week-7/customers.js
+++ practicum/week-7/customers.js
@@ -1,6 +1,8 @@
 
 let customers = [];
 
-customers.push({id: 0, name:'', email: '', password: ''});
+// When you add a new record to the customer, you should use the follwoing signature for the object
+// that you are adding to the list (id should be a unique number):
+//customers.push({id: 0, name:'', email: '', password: ''});
 
 exports.customers = customers;
--- practicum/week-7/scores.js
+++ practicum/week-7/scores.js
@@ -1,6 +1,8 @@
 
 let scores = [];
 
-scores.push({quizTaker: '', quizId: '', score: '', date: ''});
+// When you add a new record to the scores list follow the following signature 
+//(quizTaker shoudl be the email of the user and quizId is the id of the quiz):
+//scores.push({quizTaker: '', quizId: '', score: '', date: ''});
 
 exports.scores = scores;
